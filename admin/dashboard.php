5880596e3abf5f00455f58b51224566f8e9a01d5
zulaikha Moidin <zulaikha.k@gmail.com>
2016-01-12 13:14:14 -0800

    updated some admin files

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -137,62 +137,17 @@ else if($currenttab == "report") {
     <script type="text/javascript" src="../js/jquery-ui.min.js"></script>
                 <link rel="stylesheet" type="text/css" href="../css/jquery-ui.css">
                     <script type='text/javascript'>//<![CDATA[
-                        $(window).load(function(){
+                        $(document).ready(function(){
                          $( "#slider-range" ).slider({
                                     range: true,
                                     min: 0,
-                                    max: 100000,
-                                    values: [ 500, 100000 ],
+                                    max: 10000,
+                                    values: [ 50, 10000 ],
                                     slide: function( event, ui ) {  $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
                                     }
                          });
                         $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) + " - $" + $( "#slider-range" ).slider( "values", 1 ) );
                         });
-
-                          $(document).ready(function(){
-                                $(".tab1 .single-bottom").hide();
-                                $(".tab2 .single-bottom").hide();
-                                $(".tab3 .single-bottom").hide();
-                                $(".tab4 .single-bottom").hide();
-                                $(".tab5 .single-bottom").hide();
-
-                                $(".tab1 ul").click(function(){
-                                    $(".tab1 .single-bottom").slideToggle(300);
-                                    $(".tab2 .single-bottom").hide();
-                                    $(".tab3 .single-bottom").hide();
-                                    $(".tab4 .single-bottom").hide();
-                                    $(".tab5 .single-bottom").hide();
-                                })
-                                $(".tab2 ul").click(function(){
-                                    $(".tab2 .single-bottom").slideToggle(300);
-                                    $(".tab1 .single-bottom").hide();
-                                    $(".tab3 .single-bottom").hide();
-                                    $(".tab4 .single-bottom").hide();
-                                    $(".tab5 .single-bottom").hide();
-                                })
-                                $(".tab3 ul").click(function(){
-                                    $(".tab3 .single-bottom").slideToggle(300);
-                                    $(".tab4 .single-bottom").hide();
-                                    $(".tab5 .single-bottom").hide();
-                                    $(".tab2 .single-bottom").hide();
-                                    $(".tab1 .single-bottom").hide();
-                                })
-                                $(".tab4 ul").click(function(){
-                                    $(".tab4 .single-bottom").slideToggle(300);
-                                    $(".tab5 .single-bottom").hide();
-                                    $(".tab3 .single-bottom").hide();
-                                    $(".tab2 .single-bottom").hide();
-                                    $(".tab1 .single-bottom").hide();
-                                })
-                                $(".tab5 ul").click(function(){
-                                    $(".tab5 .single-bottom").slideToggle(300);
-                                    $(".tab4 .single-bottom").hide();
-                                    $(".tab3 .single-bottom").hide();
-                                    $(".tab2 .single-bottom").hide();
-                                    $(".tab1 .single-bottom").hide();
-                                });
-                            });
-
                         //]]>
                     </script>
    <?php } ?>
